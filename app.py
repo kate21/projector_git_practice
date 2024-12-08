abd8a5f9400b095c2aaaf831825f6457a1ebfdbf
Kate Ushakova <katrina21.07@gmail.com>
2024-12-08 20:52:17 +0200

    Added multiplication on a different branch

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Input - numbers and operation - sum, substraction
+#Input - numbers and operation - sum, substraction, multyplication
 
 input_data = input("Enter your operation:")
 
@@ -11,6 +11,12 @@ elif '-' in input_data:
     numbers = input_data.split("-")
     numbers = [int(num) for num in numbers]
     res = numbers[0] - sum(numbers[1:])
+    print(f'Result: {res}')
+elif '*' in input_data:
+    numbers = input_data.split("*")
+    res = 1
+    for num in numbers:
+        res *= int(num)
     print(f'Result: {res}')
 else:
     print("Operation not supportable")
