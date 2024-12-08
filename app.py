e3f9395eb393ffbbe355a6c1ccc93915a5c902e2
Kate Ushakova <katrina21.07@gmail.com>
2024-12-08 21:01:57 +0200

    division added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Input - numbers and operation - sum, substraction
+#Input - numbers and operation - sum, substraction, division
 
 input_data = input("Enter your operation:")
 
@@ -11,6 +11,13 @@ elif '-' in input_data:
     numbers = input_data.split("-")
     numbers = [int(num) for num in numbers]
     res = numbers[0] - sum(numbers[1:])
+    print(f'Result: {res}')
+elif '/' in input_data:
+    numbers = input_data.split("/")
+    numbers = [int(num) for num in numbers]
+    res = numbers[0]
+    for num in numbers[1:]:
+        res /= num
     print(f'Result: {res}')
 else:
     print("Operation not supportable")
